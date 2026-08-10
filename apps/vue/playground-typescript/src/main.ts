ead0df3902820348e03e6085f269c4ac3dc5f389
darshak-7span <darshak@7span.com>
2026-08-10 15:58:48 +0530

    Feat: give tailwind support to vuelist UI.

--- apps/vue/playground-typescript/src/main.ts
+++ apps/vue/playground-typescript/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+import ui from '@nuxt/ui/vue-plugin'
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(ui)
 app.mount('#app')
